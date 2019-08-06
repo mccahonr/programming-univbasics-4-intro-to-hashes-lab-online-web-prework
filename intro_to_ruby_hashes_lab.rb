b7dab787edae8bf6d815f587e96c704a2c8f9486
Ryan McCahon <mccahonr@yahoo.com>
2019-08-06 16:06:51 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -31,4 +31,10 @@ def update_counting_hash(hash, key)
   # given a hash an a key as parameters, return an updated hash
   # if the provided key is not present in the hash, add it and assign it to the value of 1
   # if the provided key is present, increment its value by 1
+  groceries = {"apples" => 2}
+  if groceries["apples"]
+    groceries["apples"] +=1
+  else
+    groceries[] = 1
+  end
 end
